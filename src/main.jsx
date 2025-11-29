e9033211899b7bde93bcd7a979b341689f67ae09
SchoolOS Developer <developer@schoolos.com>
2025-11-29 19:50:01 +0545

    Updated vite

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import 'leaflet/dist/leaflet.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
